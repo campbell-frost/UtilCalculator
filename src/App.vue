2a8349ff498e22558dba7b3bb55b34051a41b8e6
campbell frost <cfrost@employreward.com>
2024-03-04 17:23:14 -0500

    Removed analytics

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,11 @@
   <v-app>
     <v-main>
       <UtilCalc />
-      <inject />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
 import UtilCalc from './components/UtilCalc.vue';
-import { inject } from "@vercel/analytics"
 //
 </script>
